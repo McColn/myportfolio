0afd37728a2e775c55c913058449a62d50b9801c
McColn <cornelmtavangu1999@gmail.com>
2025-10-24 17:37:26 +0300

    oct 24 2

--- app/views.py
+++ app/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return render(request, 'app/index.html')
+
+def contact(request):
+    form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Message sent successfully, Feedback will be returned through filled Email Account')
+            return redirect('index')
+    form = ContactForm()  
+    context = {
+        'form':form,
+    }
+    return render(request, 'app/index.html', context)
